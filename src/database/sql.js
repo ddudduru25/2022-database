c980be5ad36016963f401a9984aeb06c57f5e42b
ddudduru25 <ddudduru25@gmail.com>
2022-11-03 15:42:07 +0900

    week10 upload

---
+++ src/database/sql.js
@@ -0,0 +1,39 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool(
+    process.env.JAWSDB_URL ?? {
+        host: 'localhost',
+        user: 'root',
+        //port: 3306,
+        database: 'week10',
+        password: '0428',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    }
+);
+
+const promisePool = pool.promise();
+
+export const selectSql = {
+    getUsers: async () =>{
+        const [rows] = await promisePool.query('select * from user');
+
+        return rows;
+    },
+    getDepartment: async () =>{
+        const [rows] = await promisePool.query('select * from department');
+
+        return rows;
+    },
+};
+
+export const deleteSql = {
+    deleteDepartment: async(data)=>{
+        console.log("deleteSql.deleteDepartment:", data.Dnumber);
+        const sql = 'delete from department where Dnumber=${data.Dnumber}';
+
+        await promisePool.query(sql);
+    },
+};
+
